ca0c1e641bca790802c9967f4130b7039f05aecf
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-16 02:40:46 +0200

    Config-Datei fuer alle Einstellungen hinzugefuegt

---
+++ config.lua
@@ -0,0 +1,55 @@
+-- ============================================
+-- Konfigurationsdatei Treppen-/Boden-Mechanismus
+-- Alle Werte hier anpassen, treppe_boden.lua nicht veraendern
+-- ============================================
+
+return {
+
+    -- Peripheral-Namensfragmente (peripheral.find sucht Teilstring)
+    peripherals = {
+        treppe1_ausfahren = "treppe1_ausfahren",
+        treppe1_einfahren = "treppe1_einfahren",
+        treppe2_ausfahren = "treppe2_ausfahren",
+        treppe2_einfahren = "treppe2_einfahren",
+        boden_ausfahren   = "boden_ausfahren",
+        boden_einfahren   = "boden_einfahren",
+    },
+
+    -- Bewegungsdistanzen in Bloecken
+    distanzen = {
+        treppe1 = 4,
+        treppe2 = 4,
+        boden   = 2,
+    },
+
+    -- Redstone-Wire-Connector Farbkanaele (Immersive Engineering)
+    farbkanaele = {
+        treppe1_x_eingefahren = "white",
+        treppe1_x_ausgefahren = "orange",     -- = Z eingefahren
+        treppe2_x_eingefahren = "light_blue",
+        treppe2_x_ausgefahren = "magenta",
+        boden_x_eingefahren   = "yellow",
+        boden_x_ausgefahren   = "lime",       -- = Z unten + X ausgefahren + gedreht
+    },
+
+    -- Redstone-Seiten am Computer, funktionsspezifisch
+    redstone_seiten = {
+        treppe1_z = "back",    -- Treppe1: Z-Achse, beide Richtungen
+        boden_z   = "top",     -- Boden: Z-Achse
+        boden_x   = "bottom",  -- Boden: X-Achse (mit Z zusammen fuer Drehung)
+        trigger   = "left",    -- externer Redstone-Trigger fuer Gesamtablauf
+    },
+
+    -- Zugriffskontrolle Player Detector (Advanced Peripherals)
+    spieler = {
+        reichweite = 5,
+        erlaubte_spieler = {
+            -- "Spielername1",
+            -- "Spielername2",
+        },
+    },
+
+    -- Sonstiges
+    timeout_sekunden = 15,
+
+}
