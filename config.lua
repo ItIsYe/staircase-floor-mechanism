ec5a94d6a7dd68124dad09f0671590a25d4cf1e2
Dax1 <96622640+ItIsYe@users.noreply.github.com>
2026-08-20 20:31:36 +0200

    Richtung boden_a_ausfahren umgedreht (Kalibrierung)

--- config.lua
+++ config.lua
@@ -53,7 +53,7 @@ return {
         boden_x_einfahren = 1,
         boden_z_ausfahren = 1,
         boden_z_einfahren = -1,
-        boden_a_ausfahren = -1,   -- Drehung zurueck (90 -> 0 Grad)
+        boden_a_ausfahren = 1,   -- Drehung zurueck (90 -> 0 Grad)
         boden_a_einfahren = 1,   -- Drehung (0 -> 90 Grad)
     },
 
@@ -164,6 +164,7 @@ return {
     settle_pause_sekunden = 0.25,
 
 }
+
 
 
 
